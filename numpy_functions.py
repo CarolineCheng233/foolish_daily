a0c926ca43a5487f9746974527d57c4ea6f6634b
CarolinaCheng616 <948005247@qq.com>
2020-12-03 11:36:01 +0800

    add

--- numpy_functions.py
+++ numpy_functions.py
@@ -19,9 +19,19 @@ def rand():
     array2 = np.random.rand(1, 2, 3, 4, 5)  # shape:(1, 2, 3, 4, 5)
 
 
+def random():
+    # size: int or tuple
+    # return random data in range [0.0, 1.0)
+    array1 = np.random.random()
+    array2 = np.random.random(size=1)
+    array3 = np.random.random(size=(1, 2, 3))
+    array4 = np.random.random_sample(size=(1, 2, 3))
+
+
 def foo_numpy():
     choice()
     rand()
+    random()
 
 
 if __name__ == '__main__':
